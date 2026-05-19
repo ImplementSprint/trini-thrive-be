28b5eb057288b399329a9dc7777919d8c2a4d453
ArjelAaran <arjelaaran.deocampo.cics@ust.edu.ph>
2026-05-19 13:24:37 +0800

    fix(tests): add jose ESM mock to controller specs affected by RequirePersona import

--- apps/hopecard-campaign-manager-service/src/reporting/reporting.controller.spec.ts
+++ apps/hopecard-campaign-manager-service/src/reporting/reporting.controller.spec.ts
@@ -1,3 +1,5 @@
+jest.mock('jose', () => ({ jwtVerify: jest.fn(), SignJWT: jest.fn() }));
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReportingController } from './reporting.controller';
 import { ReportingService } from './reporting.service';
